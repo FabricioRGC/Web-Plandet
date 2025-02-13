b73ceba60ecaa47ff59ed2cb816c6e146e91f510
FabricioRGC <fabricioronaldogil@gmail.com>
2025-02-13 12:07:57 -0500

    r51

--- indexpruevasmodal.php
+++ indexpruevasmodal.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body style="background-color: #65A693;">
-    <nav class="navbar navbar-expand-xxl bg-light shadow-sm sticky-top" style="background-color: #123340 !important;">
+    <nav class="navbar navbar-expand-xxl bg-light shadow-sm sticky-top" style="background: linear-gradient(45deg, #0F2A3D 60%, #4B8976 110%, black 120%) !important;">
         <div class="container-xxl d-flex justify-content-between align-items-center">
             <div class="logo-container plandet">
                 <a href="index.html" target="_blank">
